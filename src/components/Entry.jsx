fd2146a1ab6e021ba9c3ff9c1a283e7e1822d2c0
pxperfectmike <pxperfectmike@gmail.com>
2023-01-27 22:30:29 -0800

    init commit

---
+++ src/components/Entry.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import './components.css';
+
+function Entry() {
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch(
+				'https://v2.jokeapi.dev/joke/Any?safe-mode=false'
+			);
+			const json = await response.json();
+			setData(json);
+		}
+		fetchData();
+	}, []);
+
+	const handleClick = () => {
+		async function fetchData() {
+			const response = await fetch(
+				'https://v2.jokeapi.dev/joke/Any?safe-mode=true'
+			);
+			const json = await response.json();
+			setData(json);
+		}
+		fetchData();
+	};
+
+	function jokeType() {
+		if (data.type === 'single') {
+			return (
+				<>
+					<h1>{data.joke}</h1>
+				</>
+			);
+		} else {
+			return (
+				<>
+					<h1>{data.setup}</h1>
+					<h1>{data.delivery}</h1>
+				</>
+			);
+		}
+	}
+
+	return (
+		<>
+			<div className='entry'>
+				{data ? jokeType() : <h1>Loading...</h1>}
+				<button className='mainButton' onClick={handleClick}>
+					Reload Joke
+				</button>
+			</div>
+		</>
+	);
+}
+
+export default Entry;
